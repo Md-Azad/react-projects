2d411492592111bb58cc321ec30aebd4526b7117
Md Azad Hossain <sokalazad@gmail.com>
2024-10-23 20:39:52 +0200

    shown all the blog data but yet to give design

--- react_knowledge_cafe/src/components/Blog/Blog.jsx
+++ react_knowledge_cafe/src/components/Blog/Blog.jsx
@@ -1,9 +1,40 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
-  console.log(blog);
+  const {
+    cover,
+    title,
+    author_img,
+    reading_time,
+    author,
+    posted_date,
+    hashtags,
+  } = blog;
   return (
     <div>
-      <h1>{blog.title}</h1>
+      <img src={cover} alt={`cover image of ${title}`} />
+      <section className="flex justify-between">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-6">
+            <h2 className="text-2xl">{author}</h2>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <p>{reading_time} min read</p>
+        </div>
+      </section>
+
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <p>
+        {hashtags.map((tag, idx) => (
+          <span key={idx} className="mx-2 my-2">
+            {" "}
+            #{tag}
+          </span>
+        ))}
+      </p>
+      <button className="btn">Mark as read</button>
     </div>
   );
 };
